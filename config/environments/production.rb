1b5ee8bfd80087aabe677e34ecdc384fa6b8d53b
youheikanno <tkro.812.wkrni@icloud.com>
2021-05-04 15:16:44 +0900

    Fix config

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -3,6 +3,7 @@ require "active_support/core_ext/integer/time"
 Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
 
+  
   # Code is not reloaded between requests.
   config.cache_classes = true
 
@@ -30,6 +31,7 @@ Rails.application.configure do
   # Do not fallback to assets pipeline if a precompiled asset is missed.
   config.assets.compile = true
 
+  config.hosts << "simple-no-smoking.herokuapp.com"
   # Enable serving of images, stylesheets, and JavaScripts from an asset server.
   # config.asset_host = 'http://assets.example.com'
 
