4296f2bef50ae537a3a6759576921f4ac76936c8
youheikanno <tkro.812.wkrni@icloud.com>
2021-05-09 15:37:53 +0900

    Fix Production

--- config/schedule.rb
+++ config/schedule.rb
@@ -8,7 +8,7 @@
 set :output, "log/crontab.log"
 set :job_template, "/bin/zsh -l -c ':job'"
 job_type :rake, "export PATH=\"$HOME/.rbenv/bin:$PATH\"; eval \"$(rbenv init -)\"; cd :path && RAILS_ENV=:environment bundle exec rake :task :output"
-set :environment, ENV['RAILS_ENV']
+set :environment, :production
 #
 every 1.minute do
   rake "reset_reduction:reduction"
