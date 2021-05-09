9120b0c5230668a21ff2ec25f7de7077ee2458de
youheikanno <tkro.812.wkrni@icloud.com>
2021-05-10 00:29:07 +0900

    commit

--- config/application.rb
+++ config/application.rb
@@ -11,7 +11,9 @@ module SimpleNoSmoking
     config.time_zone = 'Tokyo'
     config.i18n.load_path +=
       Dir[Rails.root.join("config","locales","**","*.{rb,yml}").to_s]
-      
+    config.autoload_paths += Dir["#{config.root}/lib"]  # 追加
+    config.eager_load_paths += Dir["#{config.root}/lib/**/"]  # 追加
+    
     config.i18n.default_locale = :ja
   end
 end
