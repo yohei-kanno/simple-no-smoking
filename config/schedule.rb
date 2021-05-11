17c333fb2f211a9b47e6f6625945753774d253b1
youheikanno <tkro.812.wkrni@icloud.com>
2021-05-11 15:05:28 +0900

    Fix schejule.rb

--- config/schedule.rb
+++ config/schedule.rb
@@ -2,7 +2,7 @@ ENV['RAILS_ENV'] || :production
 
 require File.expand_path(File.dirname(__FILE__) + "/environment")
 
-set :output, "log/crontab.log"
+set :output, "#{Rails.root}/log/crontab.log"
 set :job_template, "/bin/zsh -l -c ':job'"
 job_type :rake, "export PATH=\"$HOME/.rbenv/bin:$PATH\"; eval \"$(rbenv init -)\"; cd :path && RAILS_ENV=:environment bundle exec rake :task :output"
 set :environment, ENV['RAILS_ENV']
