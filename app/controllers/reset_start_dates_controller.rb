c38426bddd8e09bb7831c4de84e79823759d142e
youheikanno <tkro.812.wkrni@icloud.com>
2021-05-11 12:51:11 +0900

    Fix: reset_start_data

--- app/controllers/reset_start_dates_controller.rb
+++ app/controllers/reset_start_dates_controller.rb
@@ -8,6 +8,8 @@ class ResetStartDatesController < ApplicationController
       
     elsif current_user.mode.mode === "reduction_mode"
       current_user.reduction_user_profile.start_date = DateTime.current
+      current_user.reduction_user_profile.smoking_count = 0
+
       flash[:remysuccess] = t(".reset_data")
       current_user.reduction_user_profile.stocks.destroy_all
       redirect_to user_reduction_user_profile_path(current_user.id) if current_user.reduction_user_profile.save! 
