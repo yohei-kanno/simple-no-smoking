4aff5985df55d3e6d82ab0c629e22ea0b60101f9
youheikanno <tkro.812.wkrni@icloud.com>
2021-05-12 11:45:36 +0900

    add: mailgun config

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -55,6 +55,7 @@ Rails.application.configure do
   config.log_level = :info
 
   host = 'simple-no-smoking.herokuapp.com'
+  config.action_mailer.raise_delivery_errors = true
 config.action_mailer.default_url_options = { host: host }
 ActionMailer::Base.smtp_settings = {
   port: ENV['MAILGUN_SMTP_PORT'],
