3bf9a368b9341e2f907317195ac02a1b07195382
yohei-kanno <74027006+yohei-kanno@users.noreply.github.com>
2021-05-13 05:45:16 +0900

    update:reduction_user_profile_show

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,28 +1,4 @@
 module ApplicationHelper
-    
-  #日付毎のカウンター
-  def stocker
-    current_user.reduction_user_profile.stocks.map do | stock |
-      stock
-    end
-  end
-  
-  def stocker_value
-    current_user.reduction_user_profile.stocks.map do | stock |
-      stock.counter
-    end
-  end
-  
-  # ユーザーの1本辺りの金額
-  def one_smoke_price
-    current_user.reduction_user_profile.tabaco_price / 20
-  end
-  
 
-  #吸った合計本数
-  def sum_value
-    stocker_value.inject(:+)
-  end
-    
   
 end
--- app/helpers/stocks_helper.rb
+++ app/helpers/stocks_helper.rb
@@ -1,2 +1,31 @@
 module StocksHelper
+      
+  #日付毎のカウンター
+  def stocker
+    current_user.reduction_user_profile.stocks.map do | stock |
+      stock
+    end
+  end
+  
+  def stocker_value
+    current_user.reduction_user_profile.stocks.map do | stock |
+      stock.counter
+    end
+  end
+  
+  # ユーザーの1本辺りの金額
+  def one_smoke_price
+    (current_user.reduction_user_profile.tabaco_price / 20.0)
+  end
+  
+
+  #吸った合計本数
+  def sum_value
+    stocker_value.inject(:+)
+  end
+  
+  #失った時給
+  
+  
+    
 end
