dcd61b14b245511ce373767c5bdd013c2ae7bdc8
yohei-kanno <74027006+yohei-kanno@users.noreply.github.com>
2021-05-18 15:03:39 +0900

    Fix: ユーザーモデルのテストコード修正

--- spec/system/users_spec.rb
+++ spec/system/users_spec.rb
@@ -1,15 +1,40 @@
 require 'rails_helper'
 
-RSpec.describe 'Users', type: :system do
+RSpec.describe '新規作成画面', type: :system do
+ 
   before do
-    @user = FactoryBot.create(:user)
+    visit new_user_path
+    fill_in "user_name", with: "kanno youhei"
+    fill_in "user_birth_date", with: Date.current
+    fill_in "user_email", with: "kanno@example.com"
+    fill_in "user_password", with: "foobar"
+    fill_in "user_password_confirmation", with: "foobar"
   end
   
-  it "新規登録画面が表示される事" do
-    visit root_path
-    expect(page).to have_button "ログイン"
-    expect(page).to have_content "メールアドレス"
-    expect(page).to have_content "パスワード"
+  context "入力が正常の場合" do
+    it "新規登録画面が表示される事" do
+      expect(page).to have_button("モード選択画面へ")
+    end
     
+    it "モード選択画面をクリックしたらユーザーが作成される事" do
+      expect{
+        find('input[ name = "commit" ]').click
+      }.to change{ User.count }.by(1)
+      expect(page).to have_content("登録が完了しました"),"フラッシュメッセージ「登録が完了しました」と表示されていません"
+    end
+    
+    it "ユーザー作成が出来たらログイン画面に遷移する事" do
+      click_button "モード選択画面へ"
+      expect(current_path).to eq(root_path),"ログイン画面に遷移出来ていません"
+    end
   end
+    
+  context "入力が未入力箇所がある場合" do
+    it "ユーザー登録が出来ない事" do
+      fill_in "user_name", with: nil
+      click_button "モード選択画面へ"
+      expect(page).to have_content("登録が出来ませんでした"),"フラッシュメッセージ「登録が出来ませんでした」と表示されていません"
+    end
+  end
+
 end
