05460514b09cf1bdf0ceb4b12a58df65f1ea1288
yohei-kanno <74027006+yohei-kanno@users.noreply.github.com>
2021-05-19 20:17:04 +0900

    Fix: viewにplaceholderを追加

--- db/schema.rb
+++ db/schema.rb
@@ -27,7 +27,7 @@ ActiveRecord::Schema.define(version: 2021_05_08_215301) do
     t.integer "hourly_wage", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
-    t.datetime "start_date", default: "2021-05-19 04:27:30"
+    t.datetime "start_date"
     t.index ["user_id"], name: "index_no_smoking_user_profiles_on_user_id"
   end
 
@@ -36,7 +36,7 @@ ActiveRecord::Schema.define(version: 2021_05_08_215301) do
     t.integer "tabaco_price", null: false
     t.integer "smoking_pace", null: false
     t.integer "hourly_wage", null: false
-    t.datetime "start_date", default: "2021-05-19 04:27:30", null: false
+    t.datetime "start_date", default: "2021-05-19 05:12:31", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.integer "smoking_count", default: 0
