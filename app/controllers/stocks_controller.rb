4dd9901ad0292c2796cc6df9aed1204178491a18
yohei-kanno <74027006+yohei-kanno@users.noreply.github.com>
2021-05-19 11:09:36 +0900

    Fix: ページネーションの掲載を逆に変更

--- app/controllers/stocks_controller.rb
+++ app/controllers/stocks_controller.rb
@@ -4,7 +4,7 @@ class StocksController < ApplicationController
   PER = 10
   
   def index
-    @stocks = current_user.reduction_user_profile.stocks.page(params[:page]).per(PER)
+    @stocks = current_user.reduction_user_profile.stocks.page(params[:page]).per(PER).reverse_order
     if current_user.reduction_user_profile.stocks.count < 1
       flash.now[:remysuccess] = t(".no_record")
     end
