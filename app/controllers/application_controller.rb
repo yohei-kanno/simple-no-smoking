da225e1417a45ced36303541bc0485adb6646171
yohei-kanno <74027006+yohei-kanno@users.noreply.github.com>
2021-05-21 01:29:22 +0900

    Fix: viewをリファクタリング

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,12 @@
 class ApplicationController < ActionController::Base
   before_action :require_login
   
+  rescue_from ActionController::RoutingError, with: :render_404
+  
+  def render_404(exception = nil)
+    redirect_to root_path
+  end
+  
   protected
   
     def not_authenticated
