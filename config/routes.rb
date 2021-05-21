10c2c644fab1115f884435749c020b65f32fb59f
yohei-kanno <74027006+yohei-kanno@users.noreply.github.com>
2021-05-22 04:47:01 +0900

    remove: admin系を削除

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,5 @@
 Rails.application.routes.draw do
   
-  mount RailsAdmin::Engine => '/admin', as: 'rails_admin'
   get "/login", to: "sessions#new"
   post "/login", to: "sessions#create"
   delete "/logout", to: "sessions#destroy"
