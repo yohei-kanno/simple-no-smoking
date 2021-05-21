fe18867a6e037bdcf994c3c5e952cdbbc8db59a0
yohei-kanno <74027006+yohei-kanno@users.noreply.github.com>
2021-05-22 03:59:20 +0900

    Fix

---
+++ app/models/ability.rb
@@ -0,0 +1,10 @@
+# class Ability
+#   include CanCan::Ability
+
+#   def initialize(user)
+#     return unless user && user&.admin?
+
+#     can :access, :rails_admin
+#     can :manage, :all
+#   end
+# end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   
+  mount RailsAdmin::Engine => '/admin', as: 'rails_admin'
   get "/login", to: "sessions#new"
   post "/login", to: "sessions#create"
   delete "/logout", to: "sessions#destroy"
