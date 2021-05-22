ff4a0fbc46ffeb0c7cfc4695982a3add25039c7b
yohei-kanno <74027006+yohei-kanno@users.noreply.github.com>
2021-05-22 10:29:11 +0900

    add: active_admin

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   
+  ActiveAdmin.routes(self)
   get "/login", to: "sessions#new"
   post "/login", to: "sessions#create"
   delete "/logout", to: "sessions#destroy"
