06200bb0bff45cb32716eb453a959f5f2cbadfc6
yohei-kanno <74027006+yohei-kanno@users.noreply.github.com>
2021-05-22 10:42:45 +0900

    remove: active_admin

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,5 @@
 Rails.application.routes.draw do
   
-  ActiveAdmin.routes(self)
   get "/login", to: "sessions#new"
   post "/login", to: "sessions#create"
   delete "/logout", to: "sessions#destroy"
--- db/schema.rb
+++ db/schema.rb
@@ -10,21 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_05_22_012704) do
-
-  create_table "active_admin_comments", force: :cascade do |t|
-    t.string "namespace"
-    t.text "body"
-    t.string "resource_type"
-    t.integer "resource_id"
-    t.string "author_type"
-    t.integer "author_id"
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
-    t.index ["author_type", "author_id"], name: "index_active_admin_comments_on_author"
-    t.index ["namespace"], name: "index_active_admin_comments_on_namespace"
-    t.index ["resource_type", "resource_id"], name: "index_active_admin_comments_on_resource"
-  end
+ActiveRecord::Schema.define(version: 2021_05_08_215301) do
 
   create_table "modes", force: :cascade do |t|
     t.integer "mode", null: false
