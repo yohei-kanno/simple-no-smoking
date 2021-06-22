ec1c98bd6be5176443d89c51d6ca2ac4003d646e
yohei-kanno <74027006+yohei-kanno@users.noreply.github.com>
2021-06-22 22:20:05 +0900

    Fix: gonの入力箇所変更

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -8,7 +8,7 @@ import Turbolinks from "turbolinks"
 import * as ActiveStorage from "@rails/activestorage"
 import "channels"
 import Rails from "@rails/ujs"
-require("user/sample")
+
 
 Rails.start()
 Turbolinks.start()
